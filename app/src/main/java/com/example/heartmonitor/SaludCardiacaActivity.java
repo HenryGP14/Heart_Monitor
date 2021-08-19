9bc22f145cbf19fabb2fa239cb09e13093a388e4
DarlynYami05 <dwverling@gmail.com>
2021-08-18 19:29:41 -0500

    Activity Salud cardiaca casi lista

---
+++ app/src/main/java/com/example/heartmonitor/SaludCardiacaActivity.java
@@ -0,0 +1,13 @@
+package com.example.heartmonitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SaludCardiacaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_saludcardiaca);
+    }
+}
