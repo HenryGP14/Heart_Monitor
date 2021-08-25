563fa3ae7cd0859731d5ec0e7c0b1b3e0c511008
Henry Pérez <dwverling@gmail.com>
2021-08-25 04:36:33 -0500

    Actividades 80% listas

---
+++ app/src/main/java/com/example/heartmonitor/ContactosActivity.java
@@ -0,0 +1,14 @@
+package com.example.heartmonitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_contactos);
+    }
+}
---
+++ app/src/main/java/com/example/heartmonitor/InformacionUsuarioActivity.java
@@ -0,0 +1,56 @@
+package com.example.heartmonitor;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.EditText;
+
+public class InformacionUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_info_usuario);
+    }
+
+    public void ontouchFotoPerfil(View view){
+
+    }
+
+    public void ontouchNombres(View view){
+        EditText nombres = new EditText(this);
+        nombres.setInputType(InputType.TYPE_CLASS_TEXT);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Cambiar nombres del paciente");
+        builder.setView(nombres);
+        builder.setPositiveButton("Aceptar", null);
+        builder.setNegativeButton("Camcelar", null);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    public void ontouchGenero(View view){
+
+    }
+
+    public void ontouchFechaNacimiento(View view){
+
+    }
+
+    public void ontouchCorreo(View view){
+
+    }
+
+    public void ontouchNombreUsuario(View view){
+
+    }
+
+    public void ontouchContrasegna(View view){
+
+    }
+}
