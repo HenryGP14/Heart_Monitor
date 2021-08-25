701b6345a1fef0a7ae7e46475b75e5ce8b8f9db7
Henry Pérez <dwverling@gmail.com>
2021-08-25 05:48:31 -0500

    interfaz de usuario para medicion y contactos lista

---
+++ app/src/main/java/com/example/heartmonitor/MedicionFrecuenciaActivity2.java
@@ -0,0 +1,14 @@
+package com.example.heartmonitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MedicionFrecuenciaActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_medicion_frec);
+    }
+}
