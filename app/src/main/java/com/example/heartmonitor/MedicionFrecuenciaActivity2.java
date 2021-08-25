a214ed5bd00638e9b07f9c7f07832db954e8adaa
Henry Pérez <dwverling@gmail.com>
2021-08-25 07:35:03 -0500

    Casi terminando el diseño de las actividades

--- app/src/main/java/com/example/heartmonitor/MedicionFrecuenciaActivity2.java
+++ app/src/main/java/com/example/heartmonitor/MedicionFrecuenciaActivity2.java
@@ -3,6 +3,9 @@ package com.example.heartmonitor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 public class MedicionFrecuenciaActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MedicionFrecuenciaActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_medicion_frec);
+    }
+
+    public void toggleMedicionAuto(View view){
+        ToggleButton toggle = findViewById(R.id.toggleMedicionAuto);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    System.out.println("Activó la medición automática");
+                }
+                else{
+                    System.out.println("Desactivó la medición automática");
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/heartmonitor/RegistrarseComoActivity.java
@@ -0,0 +1,14 @@
+package com.example.heartmonitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrarseComoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_registrarse_como);
+    }
+}
