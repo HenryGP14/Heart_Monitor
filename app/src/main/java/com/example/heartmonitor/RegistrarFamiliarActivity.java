227dce4dc4363b24a7dc801cdc5c6a7b03242104
Henry Pérez <dwverling@gmail.com>
2021-08-26 04:01:50 -0500

    Actividad login lista

---
+++ app/src/main/java/com/example/heartmonitor/RegistrarFamiliarActivity.java
@@ -0,0 +1,14 @@
+package com.example.heartmonitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrarFamiliarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_registrar_familiar);
+    }
+}
