bcb8a91cd08f552806c5d303c5f6214c6cd85a25
Henry Pérez <dwverling@gmail.com>
2021-08-27 02:34:09 -0500

    Listo

---
+++ app/src/main/java/com/example/heartmonitor/ListaPacientesActivity.java
@@ -0,0 +1,19 @@
+package com.example.heartmonitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ListaPacientesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_lista_pacientes);
+    }
+
+    public void btnIrAtras_actListaPaci(View view){
+
+    }
+}
